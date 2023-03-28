d9aafbc974a7d01524db82f3450440e213c592dd
leon <leon.boudier@etu.umontpellier.fr>
2023-03-29 01:20:40 +0200

    ajout de route test

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
+// Description: c'est le fichier principal du serveur
+
+// Importer le module express
 const express = require("express")
+
+// Importer le module http
 const http = require("http")
-const server = express()
+
+// Voici le port sur lequel le serveur va écouter
+const PORT = 2534
+
+// Créer une application express
+const app = express()
+
+// Renvoie à la requete GET de la racine le resultat "Hello World!"
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+
+// Renvoie à la requete GET de la branche /Test le resultat "Ceci est un test" avec la valeur de la variable "value"
+app.get("/Test", (req, res) => {
+    res.send("Ceci est un test, la valeur de la variable est: " + req.query.value)
+})
+
+// Ouvre le serveur sur le port 2534
+app.listen(2534, () => {
+    console.log("Server is running on port 2534: http://localhost:2534")
+})
