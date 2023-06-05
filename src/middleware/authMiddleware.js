8fcc79c6f7ec6b0a223a28d6da3b31719dae7bf9
TAIP25 <bouleon25@gmail.com>
2023-06-05 04:03:03 +0200

    Mise en prod v6

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,8 +5,14 @@ const secret = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
     // Récupère le token d'authentification
-    const token = req.cookies.authcookie;
-    console.log(req.cookies);
+    //const token = req.cookies.authcookie;
+    //console.log(req.cookies);
+    /*
+    authcookie=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1MjQ1YmQyYi0wYjI3LTRkMzktOGI4NC0yOWQ0NjMzNjNkYWEiLCJpYXQiOjE2ODU5MzAzMTYsImV4cCI6MTY4NjUzNTExNn0.eBlUPqaOOXeRheX51DAqE_qYFexI7bWyLNfcwvCtRSI; isAdmin=true
+    */
+    const token = req.headers.cookie.split('=')[1].split(';')[0];
+    console.log(token);
+
     // Vérifie si le token est valide
     jwt.verify(token, secret, (err, decodedToken) => {
         if (err) {
